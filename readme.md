215c5765ee459735b11ea03d18bd09b1c4a5503a
Rahul Khanna <khanna.mailme@gmail.com>
2020-01-21 03:48:30 +0800

    add base project for astro markdown api doc generator

---
+++ readme.md
@@ -0,0 +1,79 @@
+A library to generate a markup files documentation from joi file. The output will be in md format.
+
+Usage:
+Define your joi validator class
+
+You can install the package globally or just include them in the scripts in the package.json
+```
+    "markup-generator": "astro-markup-generator -v ./src -o ./markdown"
+```
+```
+-s is the source path of validators
+-o is the location of the markdown file will be generated
+```
+
+Example for multiple validator file
+```
+const Joi = require('joi')
+const JoiPhone = Joi.extend(require('joi-phone-number'))
+
+module.exports =  {
+    name: 'login',
+    description: 'Login API',
+    path: '/login',
+    type: 'post',
+    deprecated: false,
+    cors: true,
+    enableApiGatewayCaching: true,
+    cacheSchema: {
+      query: ['id', 'packageAsset'],
+      params: ['detailId'],
+      headers: ['auth']
+    }
+    JoiSchema: {
+      query: Joi.object({
+        id: Joi.string(),
+        packageAsset: Joi.string()
+      }).xor('id', 'packageAsset'),
+      body: Joi.object({
+        fname: Joi.string().required()
+      }),
+      params: Joi.object({
+        detailId: Joi.string().required()
+      }),
+      headers: Joi.object({
+        auth: Joi.string().required()
+      }),
+      response: {
+          200: {
+              description: "successfully login",
+              header: Joi.object().keys({
+                          Authorization: Joi.string().required()
+                      }),
+              body: Joi.object().keys({
+                  resultMessage: Joi.string().required(),
+                  resultDescription: Joi.string().required(),
+                  body: Joi.object().keys({
+                      accessToken: Joi.string().required(),
+                      refreshToken: Joi.string().required()
+                  })
+              })
+          },
+          400: {
+              description: "invalid request body",
+              body: Joi.object().keys({
+                  resultMessage: Joi.string().required(),
+                  resultDescription: Joi.string().required()
+              })
+          },
+          401: {
+              description: "invalid credential",
+              body: Joi.object().keys({
+                  resultMessage: Joi.string().required(),
+                  resultDescription: Joi.string().required()
+              })
+          }
+      }
+  }
+}
+```
---
+++ src/index.js
@@ -0,0 +1,109 @@
+#! /usr/bin/env node
+const { argv } = require('yargs') // eslint-disable-line
+  .alias('s', 'source')
+  .alias('o', 'output')
+  .demandOption(['s', 'o'])
+  .help('help')
+  .example('markdown-generator -s ./src -o ./markdown');
+
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs-extra');
+const j2s = require('hapi-joi-to-swagger');
+
+const validatorFile = argv.source;
+const outputDir = argv.output;
+
+const checkers = {
+  headers: 'header',
+  params: 'path',
+  query: 'query',
+  body: 'body'
+};
+
+const curateDocumentation = (files) => {
+  const docs = [];
+  files.forEach((file) => {
+    let markDown = '';
+    markDown += `# ${file.name}\n`;
+    markDown += `\`\`\`${file.type.toUpperCase()} ${file.path}\`\`\`\n\n`;
+    if (file.description) {
+      markDown += `### ${file.description}\n\n\n`;
+    }
+    markDown += `---\n`;
+    const { joiSchema } = file;
+    const paramsObject = {
+      headers: { title: 'Header Params', data: [] }, params: { title: 'Path Params', data: [] }, query: { title: 'Query Params', data: [] }, body: { title: 'Body Params', data: [] }
+    };
+    Object.keys(checkers).forEach((checker) => {
+      if (joiSchema[checker]) {
+        const { swagger } = j2s(joiSchema[checker]);
+        for (let key in swagger.properties) { // eslint-disable-line
+          const temp = {
+            name: key,
+            in: checkers[checker],
+            required: swagger.required ? swagger.required.includes(key) : false,
+            type: swagger.properties[key].type
+          };
+          paramsObject[checker].data.push(temp);
+        }
+      }
+    });
+    Object.keys(paramsObject).forEach((param) => {
+      if (paramsObject[param].data.length) {
+        markDown += `\`\`\`${paramsObject[param].title}\`\`\`\n`;
+        markDown += `| Params | Description |Type | Required | Sample |\n`;
+        markDown += `| ----- | ----------- |---- | -------- | ------ |\n`;
+        paramsObject[param].data.forEach((data) => {
+          markDown += `| ${data.name} |  | ${data.type} | ${!!data.required} |  |\n`;
+        });
+        markDown += '\n\n';
+      }
+    });
+    markDown += `***\nResponse Formats\n`;
+    markDown += '```\n/* Enter Success Response */\n```\n\n';
+    markDown += '```\n/* Enter Error Response */\n```';
+    docs.push(markDown);
+  });
+  return docs;
+};
+
+const init = () => {
+  glob(path.join(validatorFile, '**/*.validator.js'), (err, files) => {
+    if (err) {
+      console.log('Failed to read files');
+      process.exit(0);
+    }
+    const validationFiles = [];
+    const fileNames = [];
+    files.forEach((value) => {
+      let filename = value.replace('src/handler/', '');
+      filename = filename.replace('src/api/', '');
+      fileNames.push(filename);
+      validationFiles.push(require(path.resolve(value))); // eslint-disable-line
+    });
+
+    const docs = curateDocumentation(validationFiles);
+
+    const promises = [];
+
+    docs.forEach((doc, index) => {
+      let filename = fileNames[index];
+      const names = filename.split('/');
+      names.splice(-1, 1);
+      filename = names.join('_');
+      promises.push(fs.outputFile(`${outputDir}/apiDoc-${filename}.md`, doc, (error) => {
+        if (error) {
+          throw error;
+        }
+        return true;
+      }));
+    });
+
+    Promise.all(promises).then(() => {
+      console.log('Markdown documentation ready');
+    }).catch(() => { console.log('Failed to generate markdown documentation'); });
+  });
+};
+
+init();
